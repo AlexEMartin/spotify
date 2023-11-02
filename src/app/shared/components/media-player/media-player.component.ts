bbf34727998d5d27dcb304c52fbb379f0a4da6e9
Martín <alex.a.martin@accenture.com>
2023-11-02 17:15:20 -0300

    Routes, lazy loading, ng templates, interfaces, props and more

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit {
+  
+  mockCover:TrackModel = {
+    album: "BZRP Music Sessions",
+    cover: "https://is5-ssl.mzstatic.com/image/thumb/Features125/v4/9c/b9/d0/9cb9d017-fcf6-28c6-81d0-e9ac5b0f359e/pr_source.png/800x800cc.jpg",
+    name: "Snow Tha Product || BZRP Music Sessions #39",
+    url: "",
+    _id: 39
+  }
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
