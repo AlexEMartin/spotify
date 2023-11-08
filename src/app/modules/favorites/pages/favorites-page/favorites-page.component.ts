c74cf2de82f91d654a825b8775b18ada57dec037
Martín <alex.a.martin@accenture.com>
2023-11-08 13:13:59 -0300

    Search fix and favorites fetch

--- src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
+++ src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrackService } from '@modules/tracks/services/track.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-favorites-page',
   templateUrl: './favorites-page.component.html',
   styleUrls: ['./favorites-page.component.css']
 })
-export class FavoritesPageComponent {
+export class FavoritesPageComponent implements OnInit{
 
+  listResults$: Observable<any> = of([]);
+
+  constructor(private trackService: TrackService) { }
+
+  ngOnInit(): void {
+    this.listResults$ = this.trackService.getAllTracks$();
+  }
 }
